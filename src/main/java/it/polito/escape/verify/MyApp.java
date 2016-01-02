fbf6b85fc8c98eebba69898daa1cb7434c4ba0bd
Matteo Ciccone <mat.ciccone@gmail.com>
2016-01-02 13:28:21 +0100

    First push with barebone REST service

---
+++ src/main/java/it/polito/escape/verify/MyApp.java
@@ -0,0 +1,15 @@
+package it.polito.escape.verify;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webapi")
+public class MyApp extends Application{
+	
+	/*public Set<Class<?>> getClasses(){
+		return new HashSet<Class<?>>();
+	}*/
+}
---
+++ src/main/java/it/polito/escape/verify/MyResource.java
@@ -0,0 +1,16 @@
+package it.polito.escape.verify;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+public class MyResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String testMethod(){
+		return "It works!";
+	}
+}
