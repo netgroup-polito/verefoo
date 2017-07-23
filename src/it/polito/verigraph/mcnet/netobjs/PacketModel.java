d34701cd84c5ba1868e651e43d7509dc61cfcab4
serena.spinoso <serena.spinoso@polito.it>
2017-07-23 11:29:32 +0200

    Fix indentation (OPNFV guidelines)

--- src/it/polito/verigraph/mcnet/netobjs/PacketModel.java
+++ src/it/polito/verigraph/mcnet/netobjs/PacketModel.java
@@ -17,8 +17,8 @@ This file is part of Verigraph.
 
     You should have received a copy of the GNU Affero General Public
     License along with Verigraph.  If not, see
-    <http://www.gnu.org/licenses/>. 
-*/
+    <http://www.gnu.org/licenses/>.
+ */
 package it.polito.verigraph.mcnet.netobjs;
 
 import com.microsoft.z3.DatatypeExpr;
@@ -27,56 +27,56 @@ import com.microsoft.z3.DatatypeExpr;
  * Fields that can be configured -> "dest","body","seq","proto","emailFrom","url","options"
  */
 public class PacketModel {
-	
-	private DatatypeExpr ip_dest;
-	private Integer body;
-	private Integer seq;
-	private Integer proto;
-	private Integer emailFrom;
-	private Integer url;
-	private Integer options;
-	
-	public DatatypeExpr getIp_dest() {
-		return ip_dest;
-	}
-	public void setIp_dest(DatatypeExpr ip_dest) {
-		this.ip_dest = ip_dest;
-	}
-	public Integer getBody() {
-		return body;
-	}
-	public void setBody(Integer body) {
-		this.body = body;
-	}
-	public Integer getSeq() {
-		return seq;
-	}
-	public void setSeq(Integer seq) {
-		this.seq = seq;
-	}
-	public Integer getProto() {
-		return proto;
-	}
-	public void setProto(Integer proto) {
-		this.proto = proto;
-	}
-	public Integer getEmailFrom() {
-		return emailFrom;
-	}
-	public void setEmailFrom(Integer emailFrom) {
-		this.emailFrom = emailFrom;
-	}
-	public Integer getUrl() {
-		return url;
-	}
-	public void setUrl(Integer url) {
-		this.url = url;
-	}
-	public Integer getOptions() {
-		return options;
-	}
-	public void setOptions(Integer options) {
-		this.options = options;
-	}
+
+    private DatatypeExpr ip_dest;
+    private Integer body;
+    private Integer seq;
+    private Integer proto;
+    private Integer emailFrom;
+    private Integer url;
+    private Integer options;
+
+    public DatatypeExpr getIp_dest() {
+        return ip_dest;
+    }
+    public void setIp_dest(DatatypeExpr ip_dest) {
+        this.ip_dest = ip_dest;
+    }
+    public Integer getBody() {
+        return body;
+    }
+    public void setBody(Integer body) {
+        this.body = body;
+    }
+    public Integer getSeq() {
+        return seq;
+    }
+    public void setSeq(Integer seq) {
+        this.seq = seq;
+    }
+    public Integer getProto() {
+        return proto;
+    }
+    public void setProto(Integer proto) {
+        this.proto = proto;
+    }
+    public Integer getEmailFrom() {
+        return emailFrom;
+    }
+    public void setEmailFrom(Integer emailFrom) {
+        this.emailFrom = emailFrom;
+    }
+    public Integer getUrl() {
+        return url;
+    }
+    public void setUrl(Integer url) {
+        this.url = url;
+    }
+    public Integer getOptions() {
+        return options;
+    }
+    public void setOptions(Integer options) {
+        this.options = options;
+    }
 
 }
