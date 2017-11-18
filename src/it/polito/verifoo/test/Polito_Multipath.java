581ddf2c45abeeca4dc2adf8e970faf4553610e6
yujboss <yujboss@gmail.com>
2017-11-18 11:38:29 +0100

    Removed unnecessary files

--- src/it/polito/verifoo/test/Polito_Multipath.java
+++ src/it/polito/verifoo/test/Polito_Multipath.java
@@ -71,7 +71,7 @@ public class Polito_Multipath{
     	
     	p.resetZ3(); 	
     	//PolitoMultipathTest model = new PolitoMultipathTest(p.ctx);
-//    	Polito3nodes3hosts model = new Polito3nodes3hosts(p.ctx);
+    	//Polito3nodes3hosts model = new Polito3nodes3hosts(p.ctx);
     	Polito5nodes7hosts model = new Polito5nodes7hosts(p.ctx);
     	
     	
--- webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-    <h2>Verigraph Web Service</h2>
-    <p><a href="api/graphs">Graphs resource</a>
-    <p>Visit <a href="http://localhost:8080/verigraph/api-docs/">this link</a>
-    for a detailed documentation!
-</body>
-</html>
