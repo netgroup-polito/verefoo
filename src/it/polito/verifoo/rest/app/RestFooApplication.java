fa4245444a0c0362d2277b4a035c76180e654c3b
Raffaele <raffysommy@gmail.com>
2017-11-23 16:54:56 +0100

    Add manifest for cloudfoundry and travis autobuild

--- src/it/polito/verifoo/rest/app/RestFooApplication.java
+++ src/it/polito/verifoo/rest/app/RestFooApplication.java
@@ -4,18 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class RestFooApplication extends ResourceConfig {
-    public RestFooApplication() {
+    public RestFooApplication(@Context ServletContext context) {
     	try {
 			Process proc=Runtime.getRuntime().exec("lsb_release -d -s");
 			BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 			String distro=stdInput.readLine();
-			System.err.println(distro);
+			System.out.println(distro);
+			String fullPath = context.getRealPath("/WEB-INF/lib/jni/");
+			System.out.println(fullPath);
 			if(distro.contains("Ubuntu")){
-				System.err.println("It's looklike ubuntu");
-				int ret=Runtime.getRuntime().exec("tar -xvzpf apps/verifoo.war/WEB-INF/lib/jni/ubuntu64.tar.gz -C apps/verifoo.war/WEB-INF/lib/jni").waitFor();
+				System.out.println("It's looklike ubuntu");
+				int ret=Runtime.getRuntime().exec("tar -xvzpf "+fullPath+"ubuntu64.tar.gz -C "+fullPath).waitFor();
 				System.out.println("Unzip return with code:"+ret);
 			}
 		} catch (IOException | InterruptedException e) {
@@ -23,7 +28,7 @@ public class RestFooApplication extends ResourceConfig {
 		}
     	
         String javaLibPath = System.getProperty("java.library.path");
-        System.err.println(javaLibPath);
+        System.out.println(javaLibPath);
         // Define the package which contains the service classes.
         packages("it.polito.verifoo.rest.webservice");
     }
