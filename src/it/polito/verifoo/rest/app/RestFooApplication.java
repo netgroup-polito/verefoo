a016931d4b11e3375addf74b7ce73bdddf1c820f
Raffaele <raffysommy@gmail.com>
2017-11-30 20:53:45 +0100

    Add support for ubuntu32 and debian64 and set java lib automatic loader

--- src/it/polito/verifoo/rest/app/RestFooApplication.java
+++ src/it/polito/verifoo/rest/app/RestFooApplication.java
@@ -3,6 +3,7 @@ package it.polito.verifoo.rest.app;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.core.Context;
@@ -11,6 +12,11 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class RestFooApplication extends ResourceConfig {
     public RestFooApplication(@Context ServletContext context) {
+    	this.extractZ3Lib(context);
+        // Define the package which contains the service classes.
+        packages("it.polito.verifoo.rest.webservice");
+    }
+    private void extractZ3Lib(ServletContext context){
     	try {
 			Process proc=Runtime.getRuntime().exec("lsb_release -d -s");
 			BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
@@ -19,17 +25,38 @@ public class RestFooApplication extends ResourceConfig {
 			String fullPath = context.getRealPath("/WEB-INF/lib/jni/");
 			System.out.println(fullPath);
 			if(distro.contains("Ubuntu")){
-				System.out.println("It's looklike ubuntu");
-				int ret=Runtime.getRuntime().exec("tar -xvzpf "+fullPath+"ubuntu64.tar.gz -C "+fullPath).waitFor();
+				int ret=0;
+				if(System.getProperty("os.arch").contains("64")){
+					System.out.println("It's looklike ubuntu64");
+					ret=Runtime.getRuntime().exec("tar -xvzpf "+fullPath+"ubuntu64.tar.gz -C "+fullPath).waitFor();
+				}else{
+					System.out.println("It's looklike ubuntu32");
+					ret=Runtime.getRuntime().exec("tar -xvzpf "+fullPath+"ubuntu32.tar.gz -C "+fullPath).waitFor();
+				}
+				System.out.println("Unzip return with code:"+ret);
+			}else if(distro.contains("Debian")){
+				System.out.println("It's looklike debian");
+				int ret=Runtime.getRuntime().exec("tar -xvzpf "+fullPath+"debian64.tar.gz -C "+fullPath).waitFor();
 				System.out.println("Unzip return with code:"+ret);
 			}
+	        String javaLibPath = System.getProperty("java.library.path");
+	        System.out.println(javaLibPath);
+	        if(!javaLibPath.contains(fullPath)){
+	        	setJavaLibPath(javaLibPath+";"+fullPath);
+	        }
 		} catch (IOException | InterruptedException e) {
 			// Probably Windows :D
 		}
-    	
-        String javaLibPath = System.getProperty("java.library.path");
-        System.out.println(javaLibPath);
-        // Define the package which contains the service classes.
-        packages("it.polito.verifoo.rest.webservice");
+    }
+    private void setJavaLibPath(String path){
+			try {
+				System.setProperty("java.library.path", path );
+				Field fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
+				fieldSysPath.setAccessible( true );
+				fieldSysPath.set( null, null );
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
     }
 }
