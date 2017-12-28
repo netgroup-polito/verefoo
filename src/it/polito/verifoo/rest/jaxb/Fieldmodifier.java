36a3fcc9690aaa5a7d444d14db78366ff0435757
Antonio@PC-Antonio <varvaraantonio1994@gmail.com>
2017-12-28 16:12:32 +0100

    JAXB classes rebinding

--- src/it/polito/verifoo/rest/jaxb/Fieldmodifier.java
+++ src/it/polito/verifoo/rest/jaxb/Fieldmodifier.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.8-b130911.1802 
 // Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2017.12.27 alle 05:43:19 PM CET 
+// Generato il: 2017.12.28 alle 04:01:31 PM CET 
 //
 
 
