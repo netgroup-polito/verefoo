99d5b4ea9f7ec78b743abbe8d5a9c27b985382e3
DanieleBringhenti <daniele.bringhenti@gmail.com>
2019-04-08 16:46:01 +0200

    Added FirewallCorrectness test for future regression tests

---
+++ src/it/polito/verifoo/rest/test/TestFirewallCorrectness.java
@@ -0,0 +1,349 @@
+/**
+ * 
+ */
+package it.polito.verifoo.rest.test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.microsoft.z3.Status;
+
+import it.polito.verifoo.rest.common.BadGraphError;
+import it.polito.verifoo.rest.common.Translator;
+import it.polito.verifoo.rest.common.VerifooProxy;
+import it.polito.verifoo.rest.common.VerifooSerializer;
+import it.polito.verifoo.rest.jaxb.*;
+import it.polito.verigraph.mcnet.components.IsolationResult;
+/**
+ * 
+ * This class runs some tests in order to check the correctness of the auto-configuration module
+ *
+ */
+public class TestFirewallCorrectness {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	private VerifooSerializer test(String file) throws Exception{
+		List<Node> tmp = new ArrayList<>();
+		// create a JAXBContext capable of handling the generated classes
+        System.out.println("===========FILE " + file + "===========");
+		long beginAll=System.currentTimeMillis();
+        JAXBContext jc = JAXBContext.newInstance( "it.polito.verifoo.rest.jaxb" );
+        // create an Unmarshaller
+        Unmarshaller u = jc.createUnmarshaller();
+        SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI); 
+        Schema schema = sf.newSchema( new File( "./xsd/nfvSchema.xsd" )); 
+        u.setSchema(schema);
+        // unmarshal a document into a tree of Java content objects
+        NFV root = (NFV) u.unmarshal( new FileInputStream( file ) );
+		//long endU=System.currentTimeMillis();
+        //System.out.println("Unmarshalling -> " + ((endU-beginAll)/1000) );
+        VerifooSerializer test = new VerifooSerializer(root);
+        
+        
+        if(test.isSat()){
+        		System.out.println("SAT");
+    	}
+    	else{
+    		System.out.println("UNSAT");
+    	}
+		long endAll=System.currentTimeMillis();
+        System.out.println("Total time -> " + (endAll-beginAll)+"ms" );
+
+        return test;
+	}
+	
+	/**
+	 * This test checks if a manually configured firewall correctly blocks all the packets.
+	 */
+	@Test
+	public void testFWCorrectness01(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect01.xml"); 
+			assertTrue(result.isSat());
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	/**
+	 * This test checks if a manually configured firewall correctly - with a wrong configuration - is not able to block the packets. 
+	 * It should be UNSAT
+	 */
+	@Test
+	public void testFWCorrectness02(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect02.xml"); 
+			assertTrue(!result.isSat());
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	
+	/**
+	 * This test checks if an auto-configured firewall configures only a rule which blocks all the traffic.
+	 */
+	@Test
+	public void testFWCorrectness03(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect03.xml"); 
+			assertTrue(result.isSat());
+			List<Node> listFW = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getFunctionalType().equals(FunctionalTypes.FIREWALL)).collect(Collectors.toList());
+			assertTrue(listFW.size() == 1);
+			Node node = listFW.get(0);
+		
+			assertTrue(node.getConfiguration().getFirewall().getElements().size() == 1);
+			Elements element =node.getConfiguration().getFirewall().getElements().get(0);
+			assertTrue(element.getSource().equals("-1.-1.-1.-1"));
+			assertTrue(element.getDestination().equals("-1.-1.-1.-1"));
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	
+	/**
+	 * This test checks if an auto-configured firewall configures only a rule which blocks traffic coming ONLY forum
+	 */
+	@Test
+	public void testFWCorrectness04(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect04.xml"); 
+			assertTrue(result.isSat());
+			List<Node> listFW = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getFunctionalType().equals(FunctionalTypes.FIREWALL)).collect(Collectors.toList());
+			assertTrue(listFW.size() == 1);
+			Node node = listFW.get(0);
+		
+			assertTrue(node.getConfiguration().getFirewall().getElements().size() == 1);
+			Elements element =node.getConfiguration().getFirewall().getElements().get(0);
+			assertTrue(element.getSource().equals("10.0.0.1"));
+			assertTrue(element.getDestination().equals("-1.-1.-1.-1"));
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	
+	/**
+	 * Test about policies from clients and from server at the same time
+	 * is test checks if an auto-configured firewall configures only a rule which blocks traffic coming ONLY towards 20.*.*.*
+	 */
+	@Test
+	public void testFWCorrectness05(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect05.xml"); 
+			assertTrue(result.isSat());
+			List<Node> listFW = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getFunctionalType().equals(FunctionalTypes.FIREWALL)).collect(Collectors.toList());
+			assertTrue(listFW.size() == 1);
+			Node node = listFW.get(0);
+		
+			assertTrue(node.getConfiguration().getFirewall().getElements().size() == 1);
+			Elements element =node.getConfiguration().getFirewall().getElements().get(0);
+			assertTrue(element.getSource().equals("-1.-1.-1.-1"));
+			assertTrue(element.getDestination().equals("20.-1.-1.-1"));
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	
+	/**
+	 * Test about two alternative paths
+	 * Only a firewall configures an ALLOW rule, the other is simply in DENY mode
+	 */
+	@Test
+	public void testFWCorrectness06(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect06.xml"); 
+			assertTrue(result.isSat());
+			List<Node> listFW = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getFunctionalType().equals(FunctionalTypes.FIREWALL)).collect(Collectors.toList());
+			assertTrue(listFW.size() == 2);
+			
+			boolean correct1 = false;
+			boolean correct2 = false;
+		
+			for(Node fw : listFW) {
+				List<Elements> elements = fw.getConfiguration().getFirewall().getElements();
+				if(elements.size() == 0) {
+					correct1 = true;
+				} else if(elements.size() == 1) {
+					Elements element = elements.get(0);
+					if(element.getSource().equals("-1.-1.-1.-1") && element.getDestination().equals("10.-1.-1.-1")) {
+						correct2 = true;
+					}
+				}
+			}
+		
+			assertTrue(correct1 && correct2);
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	
+	
+	/**
+	 * Test about placement
+	 * There are two paths, but now firewall must be placed
+	 */
+	@Test
+	public void testFWCorrectness07(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect07.xml"); 
+			assertTrue(result.isSat());
+			List<Node> listFW = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getFunctionalType().equals(FunctionalTypes.FIREWALL)).collect(Collectors.toList());
+			assertTrue(listFW.size() == 0);
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	/**
+	 * Test about removal of firewall
+	 * Only a firewall is removed, because it was optional
+	 */
+	@Test
+	public void testFWCorrectness08(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect08.xml"); 
+			assertTrue(result.isSat());
+			List<Node> listFW = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getFunctionalType().equals(FunctionalTypes.FIREWALL)).collect(Collectors.toList());
+			assertTrue(listFW.size() == 2);
+			
+			boolean correct1 = false;
+			boolean correct2 = false;
+			boolean correct3 = true;
+			
+			for(Node fw : listFW) {
+				if(fw.getName().equals("node1")) {
+					correct1 = true;
+				}
+				if(fw.getName().equals("node3")) {
+					correct2 = true;
+				}
+				if(fw.getName().equals("node2")) {
+					correct3 = false;
+				}
+			}
+		
+			assertTrue(correct1 && correct2 && correct3);
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	
+	
+	/**
+	 * Test about removal of firewall
+	 * All the firewalls are removed, except one, and a client becomes directly connected to the server
+	 */
+	@Test
+	public void testFWCorrectness09(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrect09.xml"); 
+			assertTrue(result.isSat());
+			List<Node> listFW = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getFunctionalType().equals(FunctionalTypes.FIREWALL)).collect(Collectors.toList());
+			assertTrue(listFW.size() == 1);
+			
+			boolean correct = false;
+			
+			for(Node fw : listFW) {
+				if(fw.getName().equals("node2")) {
+					correct = true;
+				}
+			}
+		
+			assertTrue(correct);
+			
+			Node client = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getName().equals("nodeA")).findAny().orElse(null);
+			assertTrue(client != null);
+			assertTrue(client.getNeighbour().get(0).getName().equals("nodeB"));
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	
+	
+	/**
+	 * Important test = loops + all the end points are connected to multiple nodes
+	 */
+	@Test
+	public void testFWCorrectnessLoop(){
+		try {
+			VerifooSerializer result = test( "./testfile/FWCorrectness/FWCorrectLoop.xml"); 
+			assertTrue(result.isSat());
+			List<Node> listFW = result.getNfv().getGraphs().getGraph().get(0).getNode().stream().filter(n -> n.getFunctionalType().equals(FunctionalTypes.FIREWALL)).collect(Collectors.toList());
+			assertTrue(listFW.size() == 3);
+			
+			boolean correct1 = false;
+			boolean correct2 = false;
+			boolean correct3 = false;
+			
+			for(Node fw : listFW) {
+				List<Elements> elements = fw.getConfiguration().getFirewall().getElements();
+				if(elements.size() == 0) {
+					correct1 = true;
+				} else if(elements.size() == 1) {
+					Elements element = elements.get(0);
+					if(element.getSource().equals("-1.-1.-1.-1") && element.getDestination().equals("-1.-1.-1.-1")) {
+						correct2 = true;
+					}
+					if(element.getSource().equals("10.0.0.1") && element.getDestination().equals("-1.-1.-1.-1")) {
+						correct3 = true;
+					}
+				}
+			}
+		
+			assertTrue(correct1 && correct2 && correct3);
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+	
+	
+}
