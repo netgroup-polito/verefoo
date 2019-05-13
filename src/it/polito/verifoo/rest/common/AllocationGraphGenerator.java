e8d43e6343410a7386e647263aa790a39e0a7f9e
DanieleBringhenti <daniele.bringhenti@libero.it>
2019-05-13 15:55:13 +0200

    Updated AllocationGraphGenerator class with the new constraints

--- src/it/polito/verifoo/rest/common/AllocationGraphGenerator.java
+++ src/it/polito/verifoo/rest/common/AllocationGraphGenerator.java
@@ -3,6 +3,13 @@ package it.polito.verifoo.rest.common;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.polito.verifoo.rest.jaxb.ActionTypes;
+import it.polito.verifoo.rest.jaxb.AllocationConstraintType;
+import it.polito.verifoo.rest.jaxb.AllocationConstraints;
+import it.polito.verifoo.rest.jaxb.AllocationConstraints.AllocationConstraint;
+import it.polito.verifoo.rest.jaxb.Configuration;
+import it.polito.verifoo.rest.jaxb.Firewall;
+import it.polito.verifoo.rest.jaxb.FunctionalTypes;
 import it.polito.verifoo.rest.jaxb.Graph;
 import it.polito.verifoo.rest.jaxb.NFV;
 import it.polito.verifoo.rest.jaxb.Neighbour;
@@ -11,18 +18,26 @@ import it.polito.verifoo.rest.jaxb.Node;
 public class AllocationGraphGenerator {
 
 	private NFV nfv;
+	String initialAPIp = "20.0.0.";
 	
 	public AllocationGraphGenerator(NFV nfv) {
 		this.nfv = nfv;
 		for(Graph graph : nfv.getGraphs().getGraph()) {
 			if(graph.isServiceGraph()) {
-				generateAllocationGraph(graph);
+				List<AllocationConstraint> list = new ArrayList<AllocationConstraint>();
+				AllocationConstraints ac = nfv.getConstraints().getAllocationConstraints();
+				if(ac != null) {
+					list.addAll(ac.getAllocationConstraint());
+				}
+
+				generateAllocationGraph(graph, list);	
 			}		
 		}
+
 	}
 	
-	void generateAllocationGraph(Graph graph) {
-		String base = "AllocationPlace";
+
+	void generateAllocationGraph(Graph graph, List<AllocationConstraint> list) {
 		int counter = 1;
 		List<Node> newNodes = new ArrayList<Node>();
 		for(Node node : graph.getNode()) {
@@ -31,36 +46,71 @@ public class AllocationGraphGenerator {
 			List<Neighbour> toAdd = new ArrayList<Neighbour>();
 			
 			for(Neighbour neighbour : node.getNeighbour()) {
-				if(!neighbour.getName().startsWith("AllocationPlace")) {
-					Node other = graph.getNode().stream().filter(n -> n.getName().equals(neighbour.getName())).findFirst().orElse(null);
-					boolean bidirectional = other.getNeighbour().stream().anyMatch(n -> n.getName().equals(node.getName()));
+				if(!neighbour.getName().startsWith(initialAPIp)) {
 					
-					Node allocationPlace = new Node();
-					allocationPlace.setName(base + counter);			
-					Neighbour apNeigh = new Neighbour();
-					apNeigh.setName(allocationPlace.getName());
-					counter++;
-					
-					//node.getNeighbour().removeIf(n -> n.getName().equals(neighbour.getName()));
-					//node.getNeighbour().add(apNeigh);
-					toRemove.add(neighbour.getName());
-					toAdd.add(apNeigh);
-					
-					Neighbour nextNeigh = new Neighbour();
-					nextNeigh.setName(neighbour.getName());
-					allocationPlace.getNeighbour().add(nextNeigh);
-					
-					if(bidirectional) {
-						other.getNeighbour().removeIf(n -> n.getName().equals(node.getName()));
-						other.getNeighbour().add(apNeigh);
-						Neighbour prevNeigh = new Neighbour();
-						prevNeigh.setName(node.getName());
-						allocationPlace.getNeighbour().add(prevNeigh);
+					boolean notForbidden = true;
+					boolean fwForced = false;
+					for(AllocationConstraint ac : list) {
+						if(ac.getType().equals(AllocationConstraintType.FORBIDDEN) && 
+								(
+									(ac.getNodeA().equals(node.getName()) && ac.getNodeB().equals(neighbour.getName()) ) || 
+									(ac.getNodeB().equals(node.getName()) && ac.getNodeA().equals(neighbour.getName()) )
+								)
+							) {
+							notForbidden = false;
+						}
+						
+						if(ac.getType().equals(AllocationConstraintType.FORCED) && 
+								(
+									(ac.getNodeA().equals(node.getName()) && ac.getNodeB().equals(neighbour.getName()) ) || 
+									(ac.getNodeB().equals(node.getName()) && ac.getNodeA().equals(neighbour.getName()) )
+								)
+							) {
+							fwForced = true;
+						}
 					}
 					
-					//graph.getNode().add(allocationPlace);
+					if(notForbidden) {
+						Node other = graph.getNode().stream().filter(n -> n.getName().equals(neighbour.getName())).findFirst().orElse(null);
+						boolean bidirectional = other.getNeighbour().stream().anyMatch(n -> n.getName().equals(node.getName()));
+						
+						Node allocationPlace = new Node();
+						allocationPlace.setName(initialAPIp + counter);			
+						Neighbour apNeigh = new Neighbour();
+						apNeigh.setName(allocationPlace.getName());
+						counter++;
+						
+						//node.getNeighbour().removeIf(n -> n.getName().equals(neighbour.getName()));
+						//node.getNeighbour().add(apNeigh);
+						toRemove.add(neighbour.getName());
+						toAdd.add(apNeigh);
+						
+						Neighbour nextNeigh = new Neighbour();
+						nextNeigh.setName(neighbour.getName());
+						allocationPlace.getNeighbour().add(nextNeigh);
+						
+						if(bidirectional) {
+							other.getNeighbour().removeIf(n -> n.getName().equals(node.getName()));
+							other.getNeighbour().add(apNeigh);
+							Neighbour prevNeigh = new Neighbour();
+							prevNeigh.setName(node.getName());
+							allocationPlace.getNeighbour().add(prevNeigh);
+						}
+						
+						if(fwForced) {
+							allocationPlace.setFunctionalType(FunctionalTypes.FIREWALL);
+							Configuration confFW = new Configuration();
+							confFW.setDescription("FWDescription");
+							confFW.setName("FWConfiguration");
+							Firewall fw = new Firewall();
+							fw.setDefaultAction(ActionTypes.DENY);
+							confFW.setFirewall(fw);
+							allocationPlace.setConfiguration(confFW);
+						}
+						
+						newNodes.add(allocationPlace);
+					}
 					
-					newNodes.add(allocationPlace);
 				}
 			}	
 			
@@ -75,7 +125,6 @@ public class AllocationGraphGenerator {
 	NFV getAllocationGraph() {
 		return nfv;
 	}
-	
 	
 	
 	
