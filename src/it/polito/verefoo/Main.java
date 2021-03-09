7afc9ecbc8f609ea4f0ae48f41f21a8d2c660343
SimoneBussa <39702256+SimoneBussa@users.noreply.github.com>
2021-03-09 15:29:17 +0100

    new test cases

--- src/it/polito/verefoo/Main.java
+++ src/it/polito/verefoo/Main.java
@@ -43,7 +43,7 @@ public class Main {
 				Marshaller m = jc.createMarshaller();
 				m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 				m.setProperty(Marshaller.JAXB_NO_NAMESPACE_SCHEMA_LOCATION, "./xsd/nfvSchema.xsd");
-				VerefooSerializer test = new VerefooSerializer((NFV) u.unmarshal(new FileInputStream("./testfile/AP/Nat04.xml")));
+				VerefooSerializer test = new VerefooSerializer((NFV) u.unmarshal(new FileInputStream("./testfile/AP/Nat081.xml")));
 				if (test.isSat()) {
 					loggerResult.info("SAT");
 					loggerResult.info("----------------------OUTPUT----------------------");
