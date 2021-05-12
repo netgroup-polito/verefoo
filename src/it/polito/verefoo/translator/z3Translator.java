47848e6850a2af7bbd4925c1e7f3ac0519a1c119
DanieleBringhenti <daniele.bringhenti@gmail.com>
2021-05-12 18:53:56 +0200

    Fixed Z3 Translator

--- src/it/polito/verefoo/translator/z3Translator.java
+++ src/it/polito/verefoo/translator/z3Translator.java
@@ -22,7 +22,7 @@ public class z3Translator {
 		return "define-fun .*"+n.getName()+".*_auto_src.* Address\n  \\(ip_constructor .*\\)\\)"; 
 	}
 	public static String stringToSearchFwDestination(Node n, String nrOfRule){
-		return "define-fun .*"+n.getName()+".*_auto_dst_"+nrOfRule+".* Address\n  \\(ip_constructor .*\\)\\)";
+		return "define-fun .*"+n.getName()+".*_auto_dst_"+nrOfRule+"(\\D)* Address\n  \\(ip_constructor .*\\)\\)";
 	}
 	public static String stringToSearchFwProtocol(Node n, String nrOfRule){
 		return "define-fun .*"+n.getName()+".*_auto_proto_"+nrOfRule+".* Int\n  .*\\)";
